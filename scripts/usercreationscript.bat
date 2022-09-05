6b0f8ac7826890bf73aa05b86c60e88d45c6a322
vviton <jakubwiton22@gmail.com>
2022-09-05 11:24:24 +0200

    Created user creation script

--- scripts/usercreationscript.bat
+++ scripts/usercreationscript.bat
@@ -1,10 +1,9 @@
 @echo on
 set /p testVar=Podaj numer stanowiska
-set groupCreateCommand=net localgroup Uczniowie /add
+net localgroup Uczniowie /add
 set userNames=%testVar%_3BT_1 %testVar%_3BT_2 %testVar%_4BT_1 %testVar%_4BT_2 %testVar%_2B_2 %testVar%_4BTG_1 %testVar%_3ET_1 %testVar%_2BT_1 %testVar%_2BT_2 uczen   
 
 for %%a in (%userNames%) do ( 
- set usersAddCommand=net user %%a witaj /logonpasswordchg:yes /expires:never
- set addUserToGroup=net localgroup Uczniowie %%a /add
- echo %usersAddCommand%
+net user %%a witaj /logonpasswordchg:yes /expires:never
+net localgroup Uczniowie %%a /add
 )
