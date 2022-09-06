537e903974f23d614fdd60aed380a1291df478a6
vviton <jakubwiton22@gmail.com>
2022-09-06 07:53:11 +0200

    User creation script fix1

--- scripts/usercreationscript.bat
+++ scripts/usercreationscript.bat
@@ -4,6 +4,6 @@ net localgroup Uczniowie /add
 set userNames=%testVar%_3BT_1 %testVar%_3BT_2 %testVar%_4BT_1 %testVar%_4BT_2 %testVar%_2B_2 %testVar%_4BTG_1 %testVar%_3ET_1 %testVar%_2BT_1 %testVar%_2BT_2 uczen   
 
 for %%a in (%userNames%) do ( 
-net user %%a witaj /logonpasswordchg:yes /expires:never
+net user %%a witaj /logonpasswordchg:yes /expires:never /add
 net localgroup Uczniowie %%a /add
 )
